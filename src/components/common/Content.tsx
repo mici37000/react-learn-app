4f37df00f485a8d696dbcac41c936f44eaba9ed5
Michael Margolis <michaelmargolispa@gmail.com>
2026-02-08 15:59:54 +0200

    create navbar menu

--- src/components/common/Content.tsx
+++ src/components/common/Content.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "../modules/Home";
 import Login from "../modules/auth/Login";
 import EditProduct from "../modules/products/EditProduct";
@@ -9,15 +9,13 @@ import "./Content.scss";
 function Content() {
   return (
     <main className="content">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/edit-product" element={<EditProduct />} />
-          <Route path="/product/:id" element={<Product />} />
-          <Route path="/product-list" element={<ProductList />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/edit-product" element={<EditProduct />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/products" element={<ProductList />} />
+      </Routes>
     </main>
   );
 }
--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,7 +1,51 @@
+import {
+  AppBar,
+  AppBarSection,
+  AppBarSpacer,
+} from "@progress/kendo-react-layout";
+import { useNavigate, useLocation } from "react-router-dom";
 import "./Navbar.scss";
 
 function Navbar() {
-  return <nav>Navigation menu</nav>;
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const navItems = [
+    { label: "Home", path: "/" },
+    { label: "Products", path: "/products" },
+    { label: "Add Product", path: "/edit-product" },
+    { label: "Login", path: "/login" },
+  ];
+
+  const isActive = (path: string) => {
+    return location.pathname === path;
+  };
+
+  return (
+    <AppBar className="navbar">
+      <AppBarSection>
+        <div className="navbar-brand" onClick={() => navigate("/")}>
+          <h1 className="navbar-title">React Learn App</h1>
+        </div>
+      </AppBarSection>
+
+      <AppBarSpacer />
+
+      <AppBarSection>
+        <ul className="navbar-menu">
+          {navItems.map((item) => (
+            <li
+              key={item.path}
+              className={`navbar-item ${isActive(item.path) ? "active" : ""}`}
+              onClick={() => navigate(item.path)}
+            >
+              {item.label}
+            </li>
+          ))}
+        </ul>
+      </AppBarSection>
+    </AppBar>
+  );
 }
 
 export default Navbar;
